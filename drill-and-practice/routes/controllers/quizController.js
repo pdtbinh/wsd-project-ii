11deeb2fbd7cd67279f4feb851a212b89363cb72
pdtieubinh <tinybottles2112@gmail.com>
2023-11-02 22:10:48 +0200

    import

--- drill-and-practice/routes/controllers/quizController.js
+++ drill-and-practice/routes/controllers/quizController.js
@@ -1,6 +1,6 @@
 import { findAnswerOptionsByQuestion } from "../../services/questionAnswerOptionService.js";
 import { findQuestionById, findRandomQuestionByTopic } from "../../services/questionService.js";
-import { getAllTopics } from "../../services/topicService.js";
+import { getAllTopics, getTopicById } from "../../services/topicService.js";
 
 export const showQuizPage = async ({
   params,
@@ -56,7 +56,13 @@ export const randomQuestion = async ({
   }
 
   const rows = await findRandomQuestionByTopic(params.tId);
-  const question = rows[0];
 
+  if (rows.length <= 0) {
+    const topicRows = await getTopicById(params.id);
+    const topic = topicRows[0];
+    render("noQuestion.eta", topic);
+  }
+
+  const question = rows[0];
   response.redirect(`/quiz/${params.tId}/questions/${question.id}`);
 };
